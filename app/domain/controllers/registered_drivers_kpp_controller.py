b07ce576f5caa128b39b137c0ce09cda2c9a3bac
reyko <reyko123@mail.ru>
2023-04-08 20:38:46 +0500

    CRUD drivers model

--- app/domain/controllers/registered_drivers_kpp_controller.py
+++ app/domain/controllers/registered_drivers_kpp_controller.py
@@ -2,7 +2,9 @@ from flask import request
 from flask_restx import Resource
 
 from app.domain.dto.registered_drivers_kpp_dto import Registered_drivers_kppDto
-from app.domain.services.registered_drivers_kpp_service import (get_all_registered_drivers_kpps)
+from app.domain.services.registered_drivers_kpp_service import (
+    get_all_registered_drivers_kpps, delete_registered_drivers_kpp
+)
 
 
 api = Registered_drivers_kppDto.api
@@ -15,3 +17,10 @@ class Registered_drivers_kppList(Resource):
     @api.marshal_list_with(_registered_drivers_kpp, envelope='data')
     def get(self):        
         return get_all_registered_drivers_kpps()
+
+    @api.response(201, 'Success')
+    @api.doc(description='create a new Kpp', tags=['Kpp'])
+    @api.expect(_registered_drivers_kpp, validate=True)
+    def delete(self):
+        driver_id = request.json['id'] # тут может быть ошибка
+        return delete_registered_drivers_kpp(driver_id)
