7d80e49c1c25c6a6d073a988e61947366ee5bec8
reyko <reyko123@mail.ru>
2023-04-08 14:34:56 +0500

    1

---
+++ app/domain/dto/journal_dto.py
@@ -0,0 +1,16 @@
+from flask_restx import Namespace, fields
+from app.application import db
+
+
+
+class JournalDto:
+    api = Namespace('journal', description='')
+    journal = api.model('journal',{
+        'pass_number': fields.String(required=True),
+        'auto_number': fields.String(required=False),
+        'driver':fields.String(required=True),
+        'client_name': fields.String(required=False),
+        'goods_name' : fields.String(required=False),
+        'count': fields.Float(required=True),
+        # 'measurement_system_type': fields.String(required=False),
+    })
